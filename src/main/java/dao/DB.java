322db4c9a517aa9a0b609ac0bb12e90a6610e54f
vokumu <victoria.okumu@student.moringaschool.com>
2021-08-23 06:30:20 +0300

    udpaded bd file

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -3,7 +3,6 @@ package dao;
 import org.sql2o.Sql2o;
 
 public class DB {
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational", "emile067", "mushimiyimana");
 // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-52-20-66-171.compute-1.amazonaws.com:5432/dp5gb9n78egrb", "tobdqibdcoenom", "5f0cd838030bbe6aa16cc5b11bb5e7a19b847a9c9df0844babc82dc8e229844e");
 public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/organisational",  "denvar", "kenya@2020");
 }
